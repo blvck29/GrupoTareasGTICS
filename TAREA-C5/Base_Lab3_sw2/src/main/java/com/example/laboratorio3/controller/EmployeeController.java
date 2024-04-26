9f662d670f281f4379f9634f3cb13a4b3fc69a9b
gerardo2810 <144623841+gerardo2810@users.noreply.github.com>
2024-04-26 17:28:48 -0500

    controller

--- TAREA-C5/Base_Lab3_sw2/src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ TAREA-C5/Base_Lab3_sw2/src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,35 +1,43 @@
 package com.example.laboratorio3.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 //COMPLETAR
+@Controller
 public class EmployeeController {
 
     //COMPLETAR
-
+    @GetMapping(value = "/employee/main")
+    public String mainPage(){
+        return "employee/main";
+    }
+    @GetMapping(value = "/employee/lista")
     public String listaEmployee(   ){
         //COMPLETAR
+        return "employee/lista";
     }
-
+    @GetMapping(value = "/employee/neFrm")
     public String nuevoEmployeeForm( ) {
         //COMPLETAR
+        return "/employee/neFrm";
     }
 
-
+    @GetMapping(value = "/employee")
     public String guardarEmployee() {
-        //COMPLETAR
+        return "";
     }
 
-
+    @GetMapping(value = "/employee/editFrm")
     public String editarEmployee() {
-        //COMPLETAR
+        return "employee/editFrm";
     }
 
-
+    @GetMapping(value = "/employee")
     public String borrarEmpleado() {
-
-       //COMPLETAR
-
+        return "/employee/";
     }
 
-    //COMPLETAR
 
 }
