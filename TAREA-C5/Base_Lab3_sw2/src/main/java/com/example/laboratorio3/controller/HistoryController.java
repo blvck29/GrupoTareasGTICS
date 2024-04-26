e1b98271627a4e846b99d63542c41565225a41f7
J3anP <a20213733@pucp.edu.pe>
2024-04-26 17:53:23 -0500

    Ola2

--- TAREA-C5/Base_Lab3_sw2/src/main/java/com/example/laboratorio3/controller/HistoryController.java
+++ TAREA-C5/Base_Lab3_sw2/src/main/java/com/example/laboratorio3/controller/HistoryController.java
@@ -1,6 +1,6 @@
 package com.example.laboratorio3.controller;
 
-
+import org.springframework.ui.Model;
 import com.example.laboratorio3.repository.JobHistoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +15,10 @@ final JobHistoryRepository jobHistoryRepository;
     }
     @GetMapping(value = "/history/lista")
     public String listaHistory(){
+        return "history/lista";
+    }
+    @GetMapping(value = "/history/buscar")
+    public String buscarHistory(Model mode) {
         return "history/lista";
     }
 
