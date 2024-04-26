6a58db7e61937ec2ef0e83c4750f95ce163419c1
Pierohc <133154904+Pierohc@users.noreply.github.com>
2024-04-26 17:59:48 -0500

    aa

--- TAREA-C5/Base_Lab3_sw2/src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ TAREA-C5/Base_Lab3_sw2/src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -5,6 +5,7 @@ package com.example.laboratorio3.repository;
 import com.example.laboratorio3.entity.Employees;
 import com.example.laboratorio3.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,9 @@ import java.util.List;
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
 
     List<Employees> getEmployeesByJob(Job job);
+
+
+    @Query(value="SELECT * FROM tabla WHERE nombre LIKE 'alejand%';", nativeQuery= true )
+    List <Employees> ListaFiltrada(String word);
 
 }
